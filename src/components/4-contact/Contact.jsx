b626349b23d3ecec0b79451811304e219404eb6b
Skikri Hassane <hassaneskikri@gmail.com>
2024-08-29 01:23:49 +0100

    contact and the footer

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -3,7 +3,7 @@ import './contact.css'
 export default function Contact() {
   return (
     <div>
-      
+      <h1>Contact</h1>  
     </div>
   );
 }
--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.css'
 export default function Footer() {
   return (
     <div>
-      
+      <h1>Footer</h1>     
     </div>
   );
 }
