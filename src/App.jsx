b0016f77794179bff50d8de409bc23b86b56308b
Skikri Hassane <hassaneskikri@gmail.com>
2024-08-29 01:22:45 +0100

    add the components to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from 'react'
+import Header from "./components/1-header/Header"
+import Hero from "./components/2-hero/Hero"
+import Main from './components/3-main/Main'
+import Contact from './components/4-contact/Contact'
+import Footer from './components/5-footer/Footer'
 
 function App() {
   return (
-    <>
-    </>
+    <div className='container'>
+      <Header></Header>
+      <Hero></Hero>
+      <div className="divider"></div>
+      <Main></Main>
+      <div className="divider"></div>
+      <Contact></Contact>
+      <div className="divider"></div>
+      <Footer></Footer>
+    </div>
   );
 }
 
