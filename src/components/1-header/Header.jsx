574b1b7d7572e2b80d1d6493619ca9547ed17dd7
Skikri Hassane <hassaneskikri@gmail.com>
2024-08-29 01:23:07 +0100

    add the header

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
 import './header.css'
+import React, {useState} from 'react'
 export default function header() {
+  const [ShowModal, setShowModal] = useState(false)
+
   return (
-    <div>
-      
-    </div>
+    <header className='flex'>
+      <button className='menu icon-menu' onClick={() => {setShowModal(true)}}></button>
+      <div/>
+      <nav>
+        <ul className='flex'>
+          <li><a href="">About</a></li>
+          <li><a href="">Articles</a></li>
+          <li><a href="">Projects</a></li>
+          <li><a href="">Speaking</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </nav>
+      <button className='mode'>
+        <span className='icon-moon-o'></span>
+      </button>
+
+
+      {/* if the user press of the show modal bottom */}
+      {
+        ShowModal && (
+          <div className="fixed">
+              <ul className="modal">
+                <li><button className="icon-clear" onClick={() => {setShowModal(false)}}></button></li>
+                <li><a href="">About</a></li>               
+                <li><a href="">Articles</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Speaking</a></li>
+                <li><a href="">Contact</a></li>
+              </ul>
+          </div>
+        )
+      }
+    </header>
   );
 }
