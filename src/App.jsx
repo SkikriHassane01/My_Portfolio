6907f30ba33ce21fe3280fec9f2ba0d4f6b335ee
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-08-31 23:02:42 +0100

    update the app and the index

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import Header from "./components/1-header/Header"
-import Hero from "./components/2-hero/Hero"
-import Main from './components/3-main/Main'
-import Contact from './components/4-contact/Contact'
-import Footer from './components/5-footer/Footer'
+import { useState } from "react";
+import Header from "./components/1-header/Header";
+import Hero from "./components/2-hero/Hero";
+import Main from "./components/3-main/Main";
+import Contact from "./components/4-contact/Contact";
+import Footer from "./components/5-footer/Footer";
 
 function App() {
   return (
-    <div className='container'>
+    <div id="up" className="container">
       <Header></Header>
       <Hero></Hero>
       <div className="divider"></div>
@@ -16,10 +16,11 @@ function App() {
       <Contact></Contact>
       <div className="divider"></div>
       <Footer></Footer>
-
-      <button className='icon-keyboard_arrow_up scroll2Top'></button>
+      <a href="#up">
+        <button className="icon-keyboard_arrow_up scroll2Top"></button>
+      </a>
     </div>
   );
 }
 
-export default App
+export default App;
