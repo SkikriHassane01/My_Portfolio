c93a1f29835e081d147c937a57232694b02193db
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-02 12:56:32 +0100

    update the contact us

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -1,5 +1,10 @@
 import "./contact.css";
+import { useForm, ValidationError } from "@formspree/react";
+import Lottie from "lottie-react";
+import doneAnimation from "../../animation/done.json";
+import contactAnimation from "../../animation/contactus.json"
 export default function Contact() {
+  const [state, handleSubmit] = useForm("xgvwdgbg");
   return (
     <section className="contact-us">
       <h1 className="title">
@@ -10,21 +15,47 @@ export default function Contact() {
         Contact us for more information and Get notified when I publish
         something new.
       </p>
-      <div className="flex">
-        <form action="">
+      <div className="flex" style={{justifyContent:"space-between"}}>
+        <form onSubmit={handleSubmit}>
           <div className="flex">
             <label htmlFor="email">Email Address:</label>
-            <input type="email" id="email"required />
+            <input type="email" id="email" name="email" required />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
 
-          <div className="flex" style={{marginTop: "25px"}}>
+          <div className="flex" style={{ marginTop: "25px" }}>
             <label htmlFor="message">Your message:</label>
-            <textarea id="message" required></textarea>
+            <textarea id="message" name="message" required></textarea>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
           </div>
 
-          <button className="submit">Submit</button>
+          <button className="submit" type="submit" disabled={state.submitting}>
+            {state.submitting ? "Submitting ..." : "Submit"}
+          </button>
+          {state.succeeded && (
+            <p className="flex"
+              style={{
+                fontSize: "16px",
+                marginTop: "1.7rem",
+                color: "var(--title)",
+              }}
+            >
+             <Lottie loop={false} className="flex" style={{height:35}} animationData={doneAnimation} />
+              Your message has been sent successfully 👌
+            </p>
+          )}
         </form>
-        <div className="animation">dgs</div>
+        <div className="animation">
+          <Lottie className="flex contact-animation" style={{height:300}} animationData={contactAnimation} />
+        </div>
       </div>
     </section>
   );
