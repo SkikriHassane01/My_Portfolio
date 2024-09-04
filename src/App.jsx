190154c97ed2894f9583e80504bec6f880dd0752
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-04 11:48:00 +0100

    update the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/2-hero/Hero";
 import Main from "./components/3-main/Main";
 import Contact from "./components/4-contact/Contact";
 import Footer from "./components/5-footer/Footer";
+import Certificate from "./components/6-certificate/certificate";
 
 function App() {
   const [showScrollBtn, setshowScrollBtn] = useState(false);
@@ -12,23 +13,37 @@ function App() {
     const handleScroll = () => {
       setshowScrollBtn(window.scrollY > 300);
     };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll); // Clean up the event listener
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll); // Clean up the event listener
   }, []);
 
   return (
     <div id="up" className="container">
       <Header></Header>
-      <Hero></Hero>
+      <div id="about">
+        <Hero></Hero>
+      </div>
       <div className="divider"></div>
-      <Main></Main>
+      <div id="Project">
+        <Main></Main>
+      </div>
       <div className="divider"></div>
-      <Contact></Contact>
+      <div id="certificates">
+        <Certificate />
+      </div>
+      <div className="divider"></div>
+      <div id="contact">
+        <Contact></Contact>
+      </div>
       <div className="divider"></div>
       <Footer></Footer>
       {showScrollBtn && (
         <a href="#up">
-          <button className={`icon-keyboard_arrow_up scroll2Top ${showScrollBtn ? "visible" : ""}`}></button>
+          <button
+            className={`icon-keyboard_arrow_up scroll2Top ${
+              showScrollBtn ? "visible" : ""
+            }`}
+          ></button>
         </a>
       )}
     </div>
