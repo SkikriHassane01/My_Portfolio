5c328f8cf4fe329aaae3b4e968754a1431fec5f8
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-10 13:32:29 +0100

    add some projects

--- src/components/3-main/ProjectsData.js
+++ src/components/3-main/ProjectsData.js
@@ -5,7 +5,7 @@ export const ListProjects = [
     imgPath: "./images/watermarked.jpg",
     subtitle:
       "In this project we will be matermaking images using opencv and flask web framework. then we will deply the project on heroku.",
-    link: "https://github.com/HassaneSkikri/Image-Watermarking",
+    link: "https://github.com/SkikriHassane01/Image-Watermarking",
   },
   {
     projectTitle: "Data Science Roadmap",
@@ -13,7 +13,7 @@ export const ListProjects = [
     imgPath: "./images/Roadmap.jpg",
     subtitle:
       "Welcome to my roadmap! Here, you’ll see how I went from a beginner in data science to mastering the field. Follow my journey and explore the skills and projects that got me there",
-    link: "https://github.com/HassaneSkikri/Data-Scienc-Roadmap-from-zero-to-hero",
+    link: "https://github.com/SkikriHassane01/Data-Scienc-Roadmap-from-zero-to-hero",
   },
   {
     projectTitle: "Portfolio Website",
@@ -21,7 +21,7 @@ export const ListProjects = [
     imgPath: "./images/portfolio.png",
     subtitle:
       "Welcome to my portfolio! Here, you’ll find all the projects I’ve worked on, from AI to web development. Take a look around and see the ideas I’ve brought to life.",
-    link: "https://github.com/HassaneSkikri/Main_Portfolio_With_React",
+    link: "https://github.com/SkikriHassane01/My_Portfolio",
   },
   {
     projectTitle: "Material Management",
@@ -29,7 +29,7 @@ export const ListProjects = [
     imgPath: "./images/C_project.jpg",
     subtitle:
       "This project is written in the C programming language and focuses on creating a material management program. based on a doubly linked list.",
-    link: "https://github.com/HassaneSkikri/Material_management-C-project",
+    link: "https://github.com/SkikriHassane01/Material_management-C-project",
   },
   {
     projectTitle: "Dog Breed Classification",
@@ -37,7 +37,7 @@ export const ListProjects = [
     imgPath: "./images/dog.jpeg",
     subtitle:
       "we tackle the challenge of dog breed identification using deep learning techniques. With a growing interest in fine-grained image categorization.",
-    link: "https://github.com/HassaneSkikri/Dog-Breed-Classification-using-Transfer-learning-",
+    link: "https://github.com/SkikriHassane01/Dog-Breed-Classification-using-Transfer-learning-",
   },
   {
     projectTitle: "Poisonous Mushrooms prediction",
@@ -45,7 +45,7 @@ export const ListProjects = [
     imgPath: "./images/Poisonous.jpeg",
     subtitle:
       "predicting whether a mushroom is edible or poisonous based on its physical characteristics. This task is part of the 2024 Kaggle Playground Series",
-    link: "https://github.com/HassaneSkikri/Binary-Prediction-of-Poisonous-Mushrooms?tab=readme-ov-file",
+    link: "https://github.com/SkikriHassane01/Binary-Prediction-of-Poisonous-Mushrooms",
   },
   {
     projectTitle: "water probability prediction using H2O",
@@ -53,7 +53,7 @@ export const ListProjects = [
     imgPath: "./images/water.jpg",
     subtitle:
       "we focus on developing a machine learning model to predict the potability (drinkability) of water using a dataset that includes various chemical features of water samples",
-    link: "https://github.com/HassaneSkikri/Drinking-water-probability-prediction-using-H2O-AutoML",
+    link: "https://github.com/SkikriHassane01/Drinking-water-probability-prediction-using-H2O-AutoML",
   },
   {
     projectTitle: "PAN card tempering detection",
@@ -61,7 +61,7 @@ export const ListProjects = [
     imgPath: "./images/card.jpg",
     subtitle:
       "PAN (Permanent Account Number) card detection is a process that involves identifying and verifying PAN cards, which are issued by the Income Tax Department of India.",
-    link: "https://github.com/HassaneSkikri/PAN_card_tempering_detection",
+    link: "https://github.com/SkikriHassane01/PAN-card-tempering-detection",
   },
   {
     projectTitle: "HeathAI: Intelligent Disease Detection",
@@ -69,7 +69,7 @@ export const ListProjects = [
     imgPath: "./images/ai.jpg",
     subtitle:
       "This project aims to develop an AI platform for the quick and accurate diagnosis of major diseases. The platform will allow users to upload medical images and receive a diagnosis within minutes.",
-    link: "https://github.com/HassaneSkikri/HealthAI_Intelligent-Disease-Detection",
+    link: "https://github.com/SkikriHassane01/HealthAI_Intelligent-Disease-Detection",
   },
   {
     projectTitle: "COVID-19 Analysis Project",
@@ -77,7 +77,7 @@ export const ListProjects = [
     imgPath: "./images/covid19.jpg",
     subtitle:
       "This project focuses on analyzing COVID-19 data using SQL queries and visualizing the results using Tableau public. We will explore various aspects of the COVID-19 pandemic.",
-    link: "https://github.com/HassaneSkikri/Analyse_Covide19_with_SQL_Tableau",
+    link: "https://github.com/SkikriHassane01/Analyse_Covide19_with_SQL_Tableau",
   },
   {
     projectTitle: "To-Do Application",
@@ -85,7 +85,7 @@ export const ListProjects = [
     imgPath: "./images/todo.jpg",
     subtitle:
       "The application allows users to manage their tasks efficiently and effectively. The instance folder is reserved for the production configuration and test data.",
-    link: "https://github.com/HassaneSkikri/ToDoWithFlask",
+    link: "https://github.com/SkikriHassane01/ToDoWithFlask",
   },
   {
     projectTitle: "Vehicle Management System",
@@ -93,7 +93,7 @@ export const ListProjects = [
     imgPath: "./images/vehicule.jpg",
     subtitle:
       "This Java application is a straightforward vehicle management system that interfaces with a MySQL database to handle vehicles, clients, and rental agreements.",
-    link: "https://github.com/HassaneSkikri/Manage-Car-Reservation-Java-Project",
+    link: "https://github.com/SkikriHassane01/Manage-Car-Reservation-Java-Project",
   },
   {
     projectTitle: "Roadmap using js",
@@ -101,7 +101,7 @@ export const ListProjects = [
     imgPath: "./images/roadmapjs.png",
     subtitle:
       "Each section of the roadmap is designed to cater to diverse needs, this roadmap will help you understand the key areas of web development and guide you through your learning journey.",
-    link: "https://github.com/HassaneSkikri/Front-End-Developer-Roadmap?tab=readme-ov-file",
+    link: "https://github.com/SkikriHassane01/Webpage_Roadmap_design",
   },
   {
     projectTitle: "TicTacToe Game Java Project",
@@ -109,7 +109,7 @@ export const ListProjects = [
     imgPath: "./images/tic.jpg",
     subtitle:
       "This project is a simple implementation of the classic game Tic-Tac-Toe in Java. It's a console-based application where two players can play against each other.",
-    link: "https://github.com/HassaneSkikri/TicTacToe-Game-Java-Project",
+    link: "https://github.com/SkikriHassane01/TicTacToe-Game-Java-Project",
   },
   {
     projectTitle: "California House Pricing",
@@ -117,7 +117,7 @@ export const ListProjects = [
     imgPath: "./images/house.png",
     subtitle:
       "This project implements a linear regression model in a web application to predict outcomes based on input data. The application is written in Python and uses Flask as the web framework",
-    link: "https://github.com/HassaneSkikri/CaliforniaHousePricingEndToEndProject",
+    link: "https://github.com/SkikriHassane01/CaliforniaHousePricingEndToEndProject",
   },
   {
     projectTitle: "Store Management System",
@@ -125,14 +125,54 @@ export const ListProjects = [
     imgPath: "./images/vehicule.jpg",
     subtitle:
       "This project is a Store Management System written in C++. It is designed to manage the stock of various products in a store. It includes features for adding, updating, and removing stock items",
-    link: "https://github.com/HassaneSkikri/StoreManagementCppProject",
+    link: "https://github.com/SkikriHassane01/StoreManagementCppProject",
   },
   {
     projectTitle: "Student Exam Performance Predictor",
-    category: ["classification"],
+    category: ["regression"],
     imgPath: "./images/exam.png",
     subtitle:
       "This  end-to-end project implements a machine learning model to predict students' mathematics scores based on demographic data and scores in other subjects",
-    link: "https://github.com/HassaneSkikri/Student-Exam-Performance-Predictor-end-to-end-project",
+    link: "https://github.com/SkikriHassane01/Student-Exam-Performance-Predictor-end-to-end-project",
   },
+  {
+    projectTitle: "Laptop Price prediction",
+    category: ["regression"],
+    imgPath: "https://media.wired.com/photos/631bb97dd884b4dcc94164e3/3:2/w_2400,h_1600,c_limit/How-to-Choose-a-Laptop-Gear-GettyImages-1235728903.jpg",
+    subtitle:
+    "We will make a project for Laptop price prediction. The problem statement is that if any user wants to buy a laptop then our application should be compatible to provide a tentative price of laptop according to the user configurations",
+    link: "https://github.com/SkikriHassane01/Laptop_Price_prediction",
+  },
+  {
+    projectTitle: "Create and Deploy azure model",
+    category: ["classification"],
+    imgPath: "./images/azuredeploy.png",
+    subtitle:
+    "We will deploy a designer model as an online (real-time) endpoint in Azure Machine Learning studio.",
+    link: "https://github.com/SkikriHassane01/Create-and-Deploying-a-model-with-the-azure-ml-designer",
+  },
+  {
+    projectTitle: "Text Extraction Project",
+    category: ["Python"],
+    imgPath: "./images/text_extraction.png",
+    subtitle:
+    "Text detection and extraction play crucial roles in computer vision and natural language processing, featuring prominently in applications like optical character recognition (OCR)",
+    link: "https://github.com/SkikriHassane01/Text-Extraction-Project",
+  },
+  {
+    projectTitle: "Text Translation using Azure",
+    category: ["Python"],
+    imgPath: "./images/translation.png",
+    subtitle:
+    "Text translation is the process of converting written information from one language to another, involving the transfer of meaning while maintaining the original message's context.",
+    link: "https://github.com/SkikriHassane01/Text-Translation-using-Azure",
+  },
+  {
+    projectTitle: "Kaggle Competitions",
+    category: ["Python", "classification", "regression"],
+    imgPath: "./images/Competitions.webp",
+    subtitle:
+    " This collection represents my journey through various data science and machine learning competitions, primarily focusing on challenges from platforms like Kaggle",
+    link: "https://github.com/SkikriHassane01/Kaggle-Cometitions",
+  }
 ];
